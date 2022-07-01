bc06b2247544c49248c39d026209753455afd335
elis37 <104443314+elis37@users.noreply.github.com>
2022-07-01 08:30:17 +0500

    Update MainServer.java

--- src/main/java/MainServer.java
+++ src/main/java/MainServer.java
@@ -14,8 +14,7 @@ public class MainServer {
                  Socket clientSocket = serverSocket.accept();
                  PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
                  BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream())))
-            {
-                System.out.printf("New connection accepted Port: %d%n", clientSocket.getPort());
+            {   System.out.printf("New connection accepted Port: %d%n", clientSocket.getPort());
                 final String name = in.readLine();
                 out.println(String.format("Hi %s, your port is %d", name, clientSocket.getPort()));
             }
